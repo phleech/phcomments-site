7ae8286cb06a43104caeb2a177b63070e8cbec5e
phleech <phleech@phleech.co.uk>
2023-04-18 19:56:38 +0000

    changes made in line with changes in the phcomments project (removal of getters).

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $response = json_encode($comments);
 header('Content-Type: application/json');
 header('Content-Length: '.strlen($response));
 header('X-Comment-Count: '.count($comments));
-header('X-Max-Comment-Body-Length: '.$parser->getMaxCommentBodyLength());
-header('X-Max-Comment-Author-Length: '.$parser->getMaxCommentAuthorLength());
+header('X-Max-Comment-Body-Length: '.$parser->maxCommentBodyLength);
+header('X-Max-Comment-Author-Length: '.$parser->maxCommentAuthorLength);
 
 echo $response;
